9b29926d57696000afcd31c2822e466015591da4
emreustundag1618 <emre.ustundag.1.618@gmail.com>
2024-02-23 21:10:39 +0300

    New components added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,10 @@
 import styles from "./homepage.module.css";
 import Link from "next/link";
-import Navbar from "@/components/navbar/Navbar";
-import Footer from "@/components/footer/Footer";
 
 export default function Home() {
   return (
     <div>
       <Link href="/">
-        <Navbar/>
-        <Footer />
       </Link>
     </div>
   );
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from "./menu.module.css";
+
+const Manu = () => {
+  return (
+    <div className={styles.container}>Manu</div>
+  )
+}
+
+export default Manu
